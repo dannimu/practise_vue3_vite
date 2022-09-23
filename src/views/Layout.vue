f9123d891507b9faa34ffc1e060ed5f30db59b5c
dannimu <dannimu1598@gmail.com>
2022-09-23 10:46:08 +0400

    vue3+element-plus+vite

---
+++ src/views/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <!-- 标题 -->
+        <title-search />
+        <!-- 表格 -->
+        <content-section />
+    </div>
+</template>
+<script setup>
+import TitleSearch from '../components/TitleSearch.vue'
+import ContentSection from '../components/ContentSection.vue'
+</script>
