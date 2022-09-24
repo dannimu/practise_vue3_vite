31ca18babd0b33287e4ce8d0cd08c30b7ba39d30
dannimu <dannimu1598@gmail.com>
2022-09-24 17:25:56 +0400

    增删改

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,62 +1,21 @@
 <template>
-    <div>
-        <!-- 标题 -->
-         <div class="title-search">
-            <el-input v-model="input" placeholder="请输入姓名搜索🔍" />
-            <add-popup @dialogForm="dialogForm"/>
-        </div>
-        <!-- 表格 -->
-        <content-section :tableData="tableData"/>
-    </div>
+    <list-exercise />
 </template>
 <style lang="less" scoped>
-.title-search{
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 20px;
+.app-layout{
+    .new{
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 20px;
+    }
+    .delete{
+        color:#F56C6C;
+    }
 }
 .el-input{
-    width: 200px;
+    max-width: 300px;
 }
 </style>
 <script setup>
-import ContentSection from '../components/ContentSection.vue'
-import AddPopup from '../components/AddPopup.vue'
-import {ref} from 'vue'
-const input = ref('')
-const tableData = ref(
-    [
-        {
-            id:'1',
-            name: 'Tom',
-            email:'1234567@qq.com',
-            phone:'222222',
-            region: '常州',
-        },
-        {
-            id:'2',
-            name: 'Tom',
-            email:'1234567@qq.com',
-            phone:'222222',
-            region: '常州',
-        },
-        {
-            id:'3',
-            name: 'Tom',
-            email:'1234567@qq.com',
-            phone:'222222',
-            region: '常州',
-        },
-        {
-            id:'4',
-            name: 'Tom',
-            email:'1234567@qq.com',
-            phone:'222222',
-            region: '常州',
-        },
-    ]
-)
-function dialogForm(tableData) {
-    console.log(tableData);
-}
+import ListExercise from '../components/ListExercise.vue'
 </script>
